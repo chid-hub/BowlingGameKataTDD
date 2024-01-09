ef1326d307c58cfb23399f853ad35be3630aef04
“chid-hub” <chidhambaramcse@gmail.com>
2024-01-09 12:13:21 +0530

    Creates SwiftUI game view

---
+++ BowlingGameKataTDD/View/SwiftUI/SwiftUIBowlingGameView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIContentView.swift
+//  BowlingGameKataTDD
+//
+//  Created by LoaclUser on 08/01/24.
+//
+
+import SwiftUI
+
+struct SwiftUIBowlingGameView: View {
+    
+    var body: some View {
+            Text("Hello from SwiftUI!")
+    }
+}
+
+struct SwiftUIBowlingGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIBowlingGameView()
+    }
+}
